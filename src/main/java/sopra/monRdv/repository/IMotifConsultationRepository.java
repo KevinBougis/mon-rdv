3f500f7757206f2b01cbbed7dc0a4186b4b779c3
Eliott <eliott.cartron@orange.fr>
2020-10-27 14:50:45 +0100

    fin interfaces

---
+++ src/main/java/sopra/monRdv/repository/IMotifConsultationRepository.java
@@ -0,0 +1,9 @@
+package sopra.monRdv.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sopra.monRdv.model.MotifConsultation;
+
+public interface IMotifConsultationRepository extends JpaRepository<MotifConsultation, Long> {
+
+}
---
+++ src/main/java/sopra/monRdv/repository/IPlageHoraireRepository.java
@@ -0,0 +1,9 @@
+package sopra.monRdv.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sopra.monRdv.model.PlageHoraire;
+
+public interface IPlageHoraireRepository extends JpaRepository<PlageHoraire, Long>{
+
+}
