79d8d13094c3858b36f0ea760e2620763ee00fa5
Kevin <kevin.bougis@gmail.com>
2020-10-27 15:05:30 +0100

    debutTest Kevin

--- src/test/java/sopra/monRdv/MonRdvApplicationTests.java
+++ src/test/java/sopra/monRdv/MonRdvApplicationTests.java
@@ -1,13 +1,28 @@
 package sopra.monRdv;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import sopra.monRdv.model.CompteUtilisateur;
+import sopra.monRdv.repository.ICompteUtilisateurRepository;
+import sopra.monRdv.repository.ILieuRepository;
 
 @SpringBootTest
 class MonRdvApplicationTests {
 
+	@Autowired
+	private ICompteUtilisateurRepository userRepo;
+	@Autowired
+	private ILieuRepository lieuRepo;
+	
 	@Test
 	void contextLoads() {
+		
+		CompteUtilisateur gyneco=new CompteUtilisateur();
+		
+		
+		
 	}
 
 }
