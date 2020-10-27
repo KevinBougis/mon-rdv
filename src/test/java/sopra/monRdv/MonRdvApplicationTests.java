79bf40a10adb6cd1747595d667f475fa2dbd0496
Kevin <kevin.bougis@gmail.com>
2020-10-27 15:21:46 +0100

    DebutTestKevin1

--- src/test/java/sopra/monRdv/MonRdvApplicationTests.java
+++ src/test/java/sopra/monRdv/MonRdvApplicationTests.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import sopra.monRdv.model.CompteUtilisateur;
+import sopra.monRdv.model.Grade;
+import sopra.monRdv.model.Lieu;
+import sopra.monRdv.model.Specialite;
+import sopra.monRdv.model.TypeUtilisateur;
 import sopra.monRdv.repository.ICompteUtilisateurRepository;
 import sopra.monRdv.repository.ILieuRepository;
 
@@ -19,8 +23,11 @@ class MonRdvApplicationTests {
 	@Test
 	void contextLoads() {
 		
-		CompteUtilisateur gyneco=new CompteUtilisateur();
+		CompteUtilisateur gyneco=new CompteUtilisateur("gynecoMdp","gyneco","Ferdinand","Ahmed","ahmed.ferdinand@gmail.com",TypeUtilisateur.Praticien,Grade.Pr,Specialite.Gynecologue);
+		userRepo.save(gyneco);
 		
+		Lieu hopital=new Lieu("Pellegrin","53 rue de la rivière","3 Etage","33000","Mérignac","0556374567");
+		lieuRepo.save(hopital);
 		
 		
 	}
