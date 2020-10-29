9bab1370b470a87c8724ab8c961429ce64075e78
Eliott <eliott.cartron@orange.fr>
2020-10-29 14:55:36 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ rdv-angular/rdv-angular/src/app/Components/accueil/accueil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil-component',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil-component.component.scss']
+})
+export class AccueilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- rdv-angular/rdv-angular/src/app/app-routing.module.ts
+++ rdv-angular/rdv-angular/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccueilComponent} from './Components/accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AccueilComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
